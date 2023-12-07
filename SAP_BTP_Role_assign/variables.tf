c3045fdbbfdbccf46a78c773dff0b17c23bb9af8
BCStest <132462815+Irfanaj@users.noreply.github.com>
2023-12-07 17:55:40 +0530

    Update variables.tf

--- SAP_BTP_Role_assign/variables.tf
+++ SAP_BTP_Role_assign/variables.tf
@@ -12,16 +12,24 @@ variable "password" {
     type = string
 }
 
-variable "subaccount_admins" {
+variable "Role10" {
+    type = string
+}
+
+variable "Role13" {
+    type = string
+}
+
+variable "Z_CUSTOM_users" {
   type        = list(string)
   description = "Defines the colleagues who are added to each subaccount as subaccount administrators."
 }
 
- variable "subaccount_service_admins" {
+ variable "subaccount_service_admin_users" {
   type        = list(string)
   description = "Defines the colleagues who are added to each subaccount as subaccount service administrators."
  }
  
  variable "custom_idp" {
     type = string
- }
+}
