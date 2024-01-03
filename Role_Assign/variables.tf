c1f06ccb753951709fd3e1496f41a24fb726b72b
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-01-03 12:01:25 +0530

    Update variables.tf

--- Role_Assign/variables.tf
+++ Role_Assign/variables.tf
@@ -15,7 +15,7 @@ variable "role" {
     type = string
 }
  variable "users" {
-  type        = ;ist(string)
+  type        = list(string)
   description = "Defines the colleagues who are added to each subaccount as subaccount service administrators."
 }
  variable "identity_provider" {
