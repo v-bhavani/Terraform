5bc44f9d30643e4cecb23954c80ec041862eb9be
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-01-20 18:22:14 +0530

    Update output.tf

--- Azuretest/output.tf
+++ Azuretest/output.tf
@@ -1,5 +1,19 @@
-output "virtual_machine_ids" {
-  value = tomap({
-    for k, vm in azurerm_linux_virtual_machine.main : k => vm.id
-  })
+output "public_ip_addresses" {
+  description = "Public IP addresses assigned to the VMs"
+  value       = azurerm_public_ip.vm_public_ip[*].ip_address
+}
+
+output "vm_network_interface_ids" {
+  description = "Network Interface IDs of the created VMs"
+  value       = azurerm_network_interface.vm_nic[*].id
+}
+
+output "vm_ids" {
+  description = "IDs of the created VMs"
+  value       = azurerm_linux_virtual_machine.main[*].id
+}
+
+output "data_disk_ids" {
+  description = "IDs of the created data disks"
+  value       = azurerm_managed_disk.source1[*].id
 }
