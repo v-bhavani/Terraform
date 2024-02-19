39c1fd66337f0bc756426428bbdd0797d3c83b74
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-02-19 16:00:48 +0530

    Update outputs.tf

--- Azure_IAM1/outputs.tf
+++ Azure_IAM1/outputs.tf
@@ -1,4 +1,4 @@
 output "assigned_principal_ids" {
   description = "The IDs of the principals assigned the specified role"
-  value       = azurerm_role_assignment.role_assignment[*].principal_id
+  value       = azurerm_role_assignment.role_assignment.principal_id
 }
