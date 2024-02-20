0a7a50c690d04908f0dae303770b2d4e5ebafe36
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-02-20 12:36:13 +0530

    Update main.tf

--- azure_vnet/main.tf
+++ azure_vnet/main.tf
@@ -2,21 +2,16 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_virtual_network" "example" {
   name                = var.virtual_network_name
   address_space       = var.address_space
   location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.resource_group
 }
 
 resource "azurerm_subnet" "example" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name = var.resource_group
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = var.subnet_address_prefixes
 }
