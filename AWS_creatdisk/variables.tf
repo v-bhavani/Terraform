e79e6d8e212d3508a6c00a99f051d70f3724a664
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-03-04 20:20:31 +0530

    Update variables.tf

--- AWS_creatdisk/variables.tf
+++ AWS_creatdisk/variables.tf
@@ -10,6 +10,10 @@ variable "volume_size" {
   description = "Size of the EBS volume in GB"
 }
 
+variable "disk_type" {
+  description = "Size of the EBS volume in GB"
+}
+
 // Uncomment below if you want to use an existing snapshot
 // variable "snapshot_id" {
 //   description = "ID of the snapshot to use for the EBS volume"
