dda8f740c692f60a52069537422653fb2cb4c33c
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-06-17 20:09:16 +0530

    Delete staticipand attach to instance directory

--- staticipand attach to instance/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-region = "us-east-1"
-instance_id = "i-0bf91b7bca63f5687"
--- staticipand attach to instance/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {
-  description = "AWS region"
-}
-
-variable "instance_id" {
-  description = "ID of the EC2 instance to attach the Elastic IP"
-}
