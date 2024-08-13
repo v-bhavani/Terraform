a268268c30330d936fa72ab96532242e4c58fdd8
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2024-08-13 20:32:48 +0530

    Update main.tf

--- AZURE_MAIN/Create_disk_attach_existvm/main.tf
+++ AZURE_MAIN/Create_disk_attach_existvm/main.tf
@@ -1,1 +1,36 @@
+provider "azurerm" {
+  features {}
+}
 
+terraform {
+  backend "azurerm"{
+    resource_group_name = "cloud-demo"
+    storage_account_name = "testcloud001423"
+    container_name = "terraform"
+    key = "diskcreateandattach.tfstate"
+ }
+}
+
+# Data block to get the existing VM
+data "azurerm_virtual_machine" "existing_vm" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+}
+
+# Create a new managed disk
+resource "azurerm_managed_disk" "example" {
+  name                 = var.disk_name
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  storage_account_type = var.storage_account_type
+  create_option        = "Empty"
+  disk_size_gb         = var.disk_size_gb
+}
+
+# Attach the disk to the existing VM
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  managed_disk_id    = azurerm_managed_disk.example.id
+  virtual_machine_id = data.azurerm_virtual_machine.existing_vm.id
+  lun                = 0
+  caching            = "ReadWrite"
+}
