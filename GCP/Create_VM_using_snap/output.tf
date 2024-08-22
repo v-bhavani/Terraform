c28462a363ead230c492a691e48ee287c0d5f357
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2024-08-22 14:09:42 +0530

    Update output.tf

--- GCP/Create_VM_using_snap/output.tf
+++ GCP/Create_VM_using_snap/output.tf
@@ -6,8 +6,3 @@ output "vm_name" {
   description = "The name of the VM instance."
   value       = google_compute_instance.vm_instance.name
 }
-
-output "instance_ip" {
-  description = "The public IP address of the VM instance."
-  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
-}
