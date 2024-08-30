b1311df7cc50bd2e381b8f1f64413dfff140677a
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2024-08-30 21:14:41 +0530

    Add files via upload

---
+++ GCP/vm1/output.tf
@@ -0,0 +1,12 @@
+output "instance_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
+
+output "vm_name" {
+  description = "The name of the VM instance."
+  value       = google_compute_instance.vm_instance.name
+}
+
+output "private_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].network_ip
+}
