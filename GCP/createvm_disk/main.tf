29a78f2baed4c2d0a7437c02a691380078c1188b
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2024-09-04 15:09:36 +0530

    main.tf

--- GCP/createvm_disk/main.tf
+++ GCP/createvm_disk/main.tf
@@ -3,7 +3,7 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
- 
+
 # Configure the backend
 terraform {
   backend "gcs" {
@@ -11,16 +11,16 @@ terraform {
     prefix = "terraform/state/vmcountsoundhar1.tfstate"
   }
 }
- 
+
 # Create images from snapshots for each VM
 resource "google_compute_image" "snapshot_image" {
-  for_each       = { for vm in var.vms : vm.name => vm }
-  name           = "${each.value.name}-image"
+  for_each        = { for vm in var.vms : vm.name => vm }
+  name            = "${each.value.name}-image"
   source_snapshot = "projects/${var.project_id}/global/snapshots/${var.snapshot_name}"
-  family         = "default"
-  project        = var.project_id
+  family          = "default"
+  project         = var.project_id
 }
- 
+
 # Create disks for VMs
 resource "google_compute_disk" "data_disk" {
   for_each = {
@@ -35,53 +35,53 @@ resource "google_compute_disk" "data_disk" {
       ]
     ]) : disk.key => disk
   }
- 
+
   name  = each.key
   size  = each.value.size_gb
   zone  = each.value.zone
   type  = "pd-standard"
 }
- 
+
 # Create VM instances and attach disks
 resource "google_compute_instance" "vm_instance" {
   for_each     = { for vm in var.vms : vm.name => vm }
   name         = each.value.name
   machine_type = each.value.machine_type
   zone         = each.value.zone
- 
+
   boot_disk {
     initialize_params {
       image = google_compute_image.snapshot_image[each.key].self_link
     }
   }
- 
+
   network_interface {
     network    = var.network_name
     subnetwork = var.subnet_name
- 
+
     access_config {
       // Ephemeral public IP address
     }
   }
- 
+
   # Attach data disks to the VM
   dynamic "attached_disk" {
     for_each = {
       for key, disk in google_compute_disk.data_disk :
       key => disk
-      if split("-", key)[0] == each.value.name
+      if startswith(key, each.key)
     }
     content {
       source = attached_disk.value.self_link
       mode   = "READ_WRITE"
     }
   }
- 
+
   service_account {
     email  = var.service_account_email
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
- 
+
   tags = var.tags
 }
 
