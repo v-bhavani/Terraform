0c44ef304c7ecadada74584844528c7e80794b66
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2024-09-06 19:55:27 +0530

    Update main.tf

--- GCP/CreateVM/main.tf
+++ GCP/CreateVM/main.tf
@@ -12,16 +12,33 @@ terraform {
   }
 }
 
-# Create images from snapshots for each VM
 resource "google_compute_image" "snapshot_image" {
-  for_each        = { for vm in var.vms : vm.name => vm }
-  name            = "${each.value.name}-image"
+  for_each       = { for vm in var.vms : vm.name => vm }
+  name           = "${each.value.name}-image"
   source_snapshot = "projects/${var.project_id}/global/snapshots/${var.snapshot_name}"
-  family          = "default"
-  project         = var.project_id
+  family         = "default"
+  project        = var.project_id
 }
 
-# Create VM instances with root disk from snapshots
+resource "google_compute_disk" "data_disk" {
+  for_each = {
+    for disk in flatten([
+      for vm in var.vms : [
+        for disk in vm.disks : {
+          key      = "${vm.name}-${disk.name}"
+          size_gb  = disk.size_gb
+          zone     = vm.zone
+        }
+      ]
+    ]) : disk.key => disk
+  }
+
+  name  = each.key
+  size  = each.value.size_gb
+  zone  = each.value.zone
+  type  = "pd-standard"
+}
+
 resource "google_compute_instance" "vm_instance" {
   for_each     = { for vm in var.vms : vm.name => vm }
   name         = each.value.name
@@ -43,10 +60,22 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
+  dynamic "attached_disk" {
+    for_each = [
+      for disk in google_compute_disk.data_disk : disk if startswith(disk.name, each.value.name)
+    ]
+    content {
+      source      = attached_disk.value.self_link
+      device_name = attached_disk.value.name
+    }
+  }
+
   service_account {
     email  = var.service_account_email
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
   tags = var.tags
+
+  depends_on = [google_compute_disk.data_disk]  # Ensure disks are created first
 }
