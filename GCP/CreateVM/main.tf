7053240f745c5c4447be352f5c3aae0f96222f2e
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2024-09-20 14:49:51 +0530

    Update main.tf

--- GCP/CreateVM/main.tf
+++ GCP/CreateVM/main.tf
@@ -9,6 +9,7 @@ terraform {
   backend "gcs" {
     bucket = "terraformbackline"  # Replace with your bucket name
     prefix = "state/blackline.tfstate"
+    impersonate_service_account = "test-auth-symphony@sapspecific.iam.gserviceaccount.com"
   }
 }
 
