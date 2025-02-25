aed274100ec7267026d7bb98875eae7fa308cb1b
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2025-02-25 12:36:14 +0530

    Update main.tf

--- Module_test/modules/ebs-volume/main.tf
+++ Module_test/modules/ebs-volume/main.tf
@@ -1,17 +1,19 @@
 resource "aws_ebs_volume" "this" {
-  count             = var.volume_count
+  count = var.volume_count
+
   availability_zone = var.availability_zone
   size             = var.volume_size
   type             = var.volume_type
 
   tags = {
-    Name = "${var.instance_name}-volume-${count.index + 1}"
+    Name = "EBS-${count.index + 1}"
   }
 }
 
 resource "aws_volume_attachment" "this" {
-  count       = var.volume_count
-  device_name = "/dev/sd${char(120 + count.index)}"  # x, y, z...
-  volume_id   = aws_ebs_volume.this[count.index].id
+  count = var.volume_count
+
+  device_name = "/dev/sd${char(98 + count.index)}"  # /dev/sdb, /dev/sdc...
+  volume_id   = aws_ebs_volume.this[count].id
   instance_id = var.instance_id
 }
