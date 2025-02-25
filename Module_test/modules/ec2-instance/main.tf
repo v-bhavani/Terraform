5504931ebb2c1ebdd2533de0aadb4f55b2da6eb7
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2025-02-25 12:35:27 +0530

    Update main.tf

--- Module_test/modules/ec2-instance/main.tf
+++ Module_test/modules/ec2-instance/main.tf
@@ -1,15 +1,11 @@
 resource "aws_instance" "this" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  subnet_id       = var.subnet_id
-  security_groups = [var.security_group_id]
-  key_name        = var.key_name
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name      = var.key_name
 
   tags = {
-    Name = var.instance_name
+    Name = "EC2-${var.instance_type}"
   }
-}
-
-output "instance_id" {
-  value = aws_instance.this.id
 }
