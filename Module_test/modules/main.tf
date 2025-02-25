75199588c32830575d65600258394f9904836f1c
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2025-02-25 12:34:07 +0530

    Update main.tf

--- Module_test/modules/main.tf
+++ Module_test/modules/main.tf
@@ -2,48 +2,40 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform {
-  backend "local" {
-    path = "terraform.tfstate"
-  }
-}
-
+# Server 1 - With 2 Volumes
 module "server1" {
   source            = "./modules/ec2-instance"
-  ami_id            = var.ami_id
   instance_type     = var.instance_type
+  ami_id           = var.ami_id
   subnet_id         = var.subnet_id
   security_group_id = var.security_group_id
-  key_name         = var.key_name
-  instance_name    = "server1"
+  key_name          = var.key_name
 }
 
 module "server1_volumes" {
-  source           = "./modules/ebs-volume"
-  instance_id      = module.server1.instance_id
-  volume_size      = var.volume_size
-  volume_type      = var.volume_type
+  source            = "./modules/ebs-volume"
+  instance_id       = module.server1.instance_id
+  volume_count      = 2
+  volume_size       = var.volume_size
+  volume_type       = var.volume_type
   availability_zone = var.availability_zone
-  volume_count     = 2
-  instance_name    = "server1"
 }
 
+# Server 2 - With 3 Volumes
 module "server2" {
   source            = "./modules/ec2-instance"
-  ami_id            = var.ami_id
   instance_type     = var.instance_type
+  ami_id            = var.ami_id
   subnet_id         = var.subnet_id
   security_group_id = var.security_group_id
-  key_name         = var.key_name
-  instance_name    = "server2"
+  key_name          = var.key_name
 }
 
 module "server2_volumes" {
-  source           = "./modules/ebs-volume"
-  instance_id      = module.server2.instance_id
-  volume_size      = var.volume_size
-  volume_type      = var.volume_type
+  source            = "./modules/ebs-volume"
+  instance_id       = module.server2.instance_id
+  volume_count      = 3
+  volume_size       = var.volume_size
+  volume_type       = var.volume_type
   availability_zone = var.availability_zone
-  volume_count     = 3
-  instance_name    = "server2"
 }
