0fdd3bcdc8af49b8a77aed8dff4b7490462c615e
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2025-06-19 17:02:42 +0530

    Update main.tf

--- AZURE_Modules/dev/vnet/main.tf
+++ AZURE_Modules/dev/vnet/main.tf
@@ -8,12 +8,12 @@ module "app1_vnet" {
   tags                = var.app1_tags
 }
 
-# module "app2_vnet" {
-#   source              = "../../modules/vnet"
-#   vnet_name           = var.app2_vnet_name
-#   resource_group_name = var.app2_rg_name
-#   location            = var.location
-#   address_space       = var.app2_address_space
-#   subnets             = var.app2_subnets
-#   tags                = var.app2_tags
-# }
+module "app2_vnet" {
+  source              = "../../modules/vnet"
+  vnet_name           = var.app2_vnet_name
+  resource_group_name = var.app2_rg_name
+  location            = var.location
+  address_space       = var.app2_address_space
+  subnets             = var.app2_subnets
+  tags                = var.app2_tags
+}
