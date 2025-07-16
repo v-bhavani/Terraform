fd9fd8fa8d10fab5f657abe7aa83537690bdb72e
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2025-07-16 19:11:28 +0530

    Create variable.tf

---
+++ Testing/variable.tf
@@ -0,0 +1,27 @@
+variable "resourcegroup" {
+  type        = string
+}
+variable "location" {
+  type        = string
+}
+variable "diskname" {
+  type        = string
+}
+variable "st_acc_type" {
+  type        = string
+}
+variable "disktype" {
+  type        = number
+}
+variable "project" {
+  description = "name of resource"
+  type        = string
+}
+variable "duration" {
+  description = "name of resource"
+  type        = string
+}
+variable "owner" {
+  description = "name of resource"
+  type        = string
+}
